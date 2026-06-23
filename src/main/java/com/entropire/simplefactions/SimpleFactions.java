b8af35ab84eff0374331a8b25a28c581ce38d3c4
Entropire <158569522+Entropire@users.noreply.github.com>
2026-06-23 14:50:45 +0200

    Initialize project

---
+++ src/main/java/com/entropire/simplefactions/SimpleFactions.java
@@ -0,0 +1,16 @@
+package com.entropire.simplefactions;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SimpleFactions extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
