0b2eb202faf1c17304a14501b3254bcfdacd9f7f
Entropire <158569522+Entropire@users.noreply.github.com>
2026-07-01 15:13:56 +0200

    Add faction object null checks

--- src/main/java/com/entropire/simplefactions/db/faction/Faction.java
+++ src/main/java/com/entropire/simplefactions/db/faction/Faction.java
@@ -11,4 +11,18 @@ public record Faction(
         Instant createdAt,
         boolean isActive,
         Instant lastActivity
-) {}
+) {
+    public Faction {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("name cannot be null or blank");
+        }
+
+        if (color == null || color.isBlank() || color.matches("^#[a-fA-F0-9]{6}$")) {
+            throw new IllegalArgumentException("color cannot be null or blank");
+        }
+
+        if (ownerUUID == null) {
+            throw new IllegalArgumentException("ownerUUID cannot be null");
+        }
+    }
+}
