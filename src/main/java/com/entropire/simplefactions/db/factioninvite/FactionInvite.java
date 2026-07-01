ff4bd11ddf13b4ee06a19efa0a234025d4807027
Entropire <158569522+Entropire@users.noreply.github.com>
2026-07-01 15:38:39 +0200

    Add FactionInvite object, repository, service

---
+++ src/main/java/com/entropire/simplefactions/db/factioninvite/FactionInvite.java
@@ -0,0 +1,28 @@
+package com.entropire.simplefactions.db.factioninvite;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public record FactionInvite(
+        UUID uuid,
+        UUID factionUuid,
+        UUID playerUuid,
+        UUID invitedBy,
+        Instant invitedAt,
+        Instant expiresAt
+) {
+    public FactionInvite {
+        if (factionUuid == null) {
+            throw new IllegalArgumentException("factionUuid cannot be null");
+        }
+        if (playerUuid == null) {
+            throw new IllegalArgumentException("playerUuid cannot be null");
+        }
+        if (invitedBy == null) {
+            throw new IllegalArgumentException("invitedBy cannot be null");
+        }
+        if (expiresAt == null) {
+            throw new IllegalArgumentException("expiresAt cannot be null");
+        }
+    }
+}
---
+++ src/main/java/com/entropire/simplefactions/db/factioninvite/exception/FactionInviteException.java
@@ -0,0 +1,7 @@
+package com.entropire.simplefactions.db.factioninvite.exception;
+
+public class FactionInviteException extends Exception{
+    public FactionInviteException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
